c2be2f355efb116216701f114825fb71f3d7b6e5
mjclaypool <mallory.claypool@gmail.com>
2024-10-01 11:16:22 -0400

    Add ability to minimize the NavBar

--- src/UI/NavBar.tsx
+++ src/UI/NavBar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import NavItem from './NavItem';
 
 import logo from '../assets/images/logo-large.svg';
+import logoSm from '../assets/images/logo-small.svg';
 import overviewIcon from '../assets/images/icon-nav-overview.svg';
 import transactionsIcon from '../assets/images/icon-nav-transactions.svg';
 import budgetsIcon from '../assets/images/icon-nav-budgets.svg';
@@ -24,6 +25,7 @@ type navItem = {
 
 const NavBar = (props: {onNavSelection: (item: string) => void}) => {
   const [activePage, setActivePage] = useState("Overview");
+  const [miniNav, setMiniNav] = useState(false);
 
   const navMenu: navItem[] = [
     {
@@ -58,31 +60,61 @@ const NavBar = (props: {onNavSelection: (item: string) => void}) => {
     }
   ]
 
-  function handleClick (menuLabel: string) {
+  function handleClick(menuLabel: string) {
     setActivePage(menuLabel);
     props.onNavSelection(menuLabel);
     window.scrollTo(0, 0);
   }
 
+  function handleMinimize() {
+    setMiniNav(true);
+  }
+
+  function handleMaximize() {
+    setMiniNav(false);
+  }
+
   return (
-    <div className="sticky bottom-0 xl:relative font-pubSans bg-p-grey900 rounded-t-xl pt-100 xl:min-h-[100vh] xl:w-[300px] xl:rounded-l-none xl:rounded-r-xl">
-      <img src={logo} alt="Finance logo" className="hidden xl:block xl:fixed p-400" />
-      <div className="flex justify-between items-center xl:fixed xl:top-[125px] xl:flex-col xl:justify-start xl:items-start md:gap-[42px] xl:gap-0 px-200 md:px-500 xl:px-0">
-        {navMenu.map((menuItem) => (
-          <div key={menuItem.label} className="flex-1 xl:flex-none" onClick={() => handleClick(menuItem.label)}>
-            {menuItem.label == activePage ?
-              <NavItem type="active" icon={menuItem.activeIcon} alt={menuItem.alt} label={menuItem.label} />
-            :
-              <NavItem type="inactive" icon={menuItem.icon} alt={menuItem.alt} label={menuItem.label} />
-            }
+    <>
+      {!miniNav ?
+        <div className="sticky bottom-0 xl:relative font-pubSans bg-p-grey900 rounded-t-xl pt-100 xl:min-h-[100vh] xl:w-[300px] xl:rounded-l-none xl:rounded-r-xl">
+          <img src={logo} alt="Finance logo" className="hidden xl:block xl:fixed p-400" />
+          <div className="flex justify-between items-center xl:fixed xl:top-[125px] xl:flex-col xl:justify-start xl:items-start md:gap-[42px] xl:gap-0 px-200 md:px-500 xl:px-0">
+            {navMenu.map((menuItem) => (
+              <div key={menuItem.label} className="flex-1 xl:flex-none" onClick={() => handleClick(menuItem.label)}>
+                {menuItem.label == activePage ?
+                  <NavItem type="active" icon={menuItem.activeIcon} alt={menuItem.alt} label={menuItem.label} size="default" />
+                :
+                  <NavItem type="inactive" icon={menuItem.icon} alt={menuItem.alt} label={menuItem.label} size="default" />
+                }
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="hidden xl:flex items-center gap-4 fixed bottom-[58px] px-400 cursor-pointer">
-        <img src={minMenuIcon} alt="Minimize menu icon" />
-        <p className="text-preset3 text-p-grey300">Minimize Menu</p>
-      </div>
-    </div>
+          <div className="hidden xl:flex items-center gap-4 fixed bottom-[58px] px-400 cursor-pointer" onClick={handleMinimize}>
+            <img src={minMenuIcon} alt="Minimize menu icon" />
+            <p className="text-preset3 text-p-grey300">Minimize Menu</p>
+          </div>
+        </div>
+      :
+        <div className="hidden xl:flex relative font-pubSans bg-p-grey900 pt-100 min-h-[100vh] w-[88px] rounded-r-xl">
+          <img src={logoSm} alt="Finance logo" className="block fixed p-400" />
+          <div className="flex flex-col justify-start items-start fixed top-[125px]">
+            {navMenu.map((menuItem) => (
+              <div key={menuItem.label} onClick={() => handleClick(menuItem.label)}>
+                {menuItem.label == activePage ?
+                  <NavItem type="active" icon={menuItem.activeIcon} alt={menuItem.alt} label={menuItem.label} size="mini" />
+                :
+                  <NavItem type="inactive" icon={menuItem.icon} alt={menuItem.alt} label={menuItem.label} size="mini" />
+                }
+              </div>
+            ))}
+          </div>
+          <div className="fixed bottom-[58px] px-400 cursor-pointer rotate-180" onClick={handleMaximize}>
+            <img src={minMenuIcon} alt="Minimize menu icon" />
+          </div>
+        </div>
+      }
+    </>
   )
 }
 
--- src/UI/NavItem.tsx
+++ src/UI/NavItem.tsx
@@ -1,14 +1,22 @@
-const NavItem = (props: {type: string, icon: string, alt: string, label: string}) => {
+const NavItem = (props: {type: string, icon: string, alt: string, label: string, size: string}) => {
   const typeStyle: {[key: string]: string} = {
     active: "text-p-grey900 bg-p-beige100 rounded-t-xl border-b-4 border-b-s-green xl:rounded-tl-none xl:border-b-0 xl:rounded-r-xl xl:border-l-4 xl:border-l-s-green",
     inactive: "text-p-grey300 border-b-4 border-b-transparent xl:border-b-0 xl:border-l-4 xl:border-l-transparent cursor-pointer"
   }
 
   return (
-    <div className={`${typeStyle[props.type]} flex flex-col xl:flex-row xl:gap-4 flex-1 xl:flex-none items-center xl:w-[276px] xl:h-[56px] py-100 xl:py-0 xl:pl-8`}>
-      <img src={props.icon} alt={props.alt} className="h-6 w-6 object-cover p-[3px]" />
-      <p className="hidden md:block text-preset5 xl:text-preset3 font-bold mt-1">{props.label}</p>
-    </div>
+    <>
+    {props.size == "default" ?
+      <div className={`${typeStyle[props.type]} flex flex-col xl:flex-row xl:gap-4 flex-1 xl:flex-none items-center xl:w-[276px] xl:h-[56px] py-100 xl:py-0 xl:pl-8`}>
+        <img src={props.icon} alt={props.alt} className="h-6 w-6 object-cover p-[3px]" />
+        <p className="hidden md:block text-preset5 xl:text-preset3 font-bold mt-1">{props.label}</p>
+      </div>
+    :
+      <div className={`${typeStyle[props.type]} flex flex-row justify-center gap-4 items-center w-[76px] h-[56px]`}>
+        <img src={props.icon} alt={props.alt} className="h-6 w-6 object-cover p-[3px]" />
+      </div>
+    }
+    </>
   )
 }
 
